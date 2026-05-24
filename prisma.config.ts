862892dab06f778c5bc8a75886b7bc3ef794aa2c
Nikhila <vattikuntans@gmail.com>
2026-05-24 18:16:04 +0530

    Fixed prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,13 +1,5 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
 
-export default defineConfig({
+export default {
   schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
-  engine: "binary",
-  datasource: {
-    url: env("DATABASE_URL"),
-  },
-});
+};
